c59b382004a7c6ed4ccfb339e077b038197fd458
Karthick Perumal <Karamya@users.noreply.github.com>
2016-10-24 16:58:58 +0200

    Update decorator_fibonacci.py

--- decorator_fibonacci.py
+++ decorator_fibonacci.py
@@ -4,6 +4,8 @@ Created on Mon Oct 24 15:53:50 2016
 
 @author: perumal
 """
+import time
+
 def memoize(f):
     memo = {}
     def helper(x):
@@ -20,14 +22,23 @@ def fib(n):
         return 1
     else:
         return fib(n-1) + fib(n-2)   ### Here also the redefined function is called (memoize(fib))
-    
+ 
+original_fib = fib   
 fib = memoize(fib)  ## decorate function which calls the helper function
 
-print(fib(40))
+x = time.time()
+for i in range(1000000):
+    fib(100)
+print('time taken for memoize function is ', time.time()-x, "seconds")
+
+y = time.time()
+for i in range(1000000):
+    original_fib(100)
+print('time taken for original fibonacci function is ', time.time()-y, "seconds")
 
 
-"""
-powerful function to avoid calculating the function multiple times. 
+
+""" powerful function to avoid calculating the function multiple times. 
 In case, of fibonacci series, it does not perform the calculation of fib(n-1) or fib(n-2) everytime 
 
 For example, when doing webscraping, if the website url is already checked, 
